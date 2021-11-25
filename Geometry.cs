032901221335481361a67e8c5e75a250f8e8b4be
Felix <felix018237@gmail.com>
2021-11-26 00:06:56 +0600

    Square Figure

---
+++ Geometry.cs
@@ -0,0 +1,22 @@
+﻿namespace Площадь
+{
+    class Geometry
+    {
+        public double Triangle(double b,double h)
+        {
+            b = b / 2;
+            return b * h;
+        }
+        public int Square(int a)
+        {
+            return a * a;
+        }
+        public int Rectangle(int a,int b)
+        {
+            return a * b;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
